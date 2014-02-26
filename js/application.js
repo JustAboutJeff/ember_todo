ff88a6266289f62fcff8b73d59e8b7c8fe9ea128
Jeff Belser <jbelser@instructure.com>
2014-02-25 18:53:45 -0600

    add filter route handling for active and done todos

--- js/application.js
+++ js/application.js
@@ -2,12 +2,43 @@ window.App = Ember.Application.create({});
 
 // === ROUTES ===
 App.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+    this.route('active');
+    this.route('done');
+  });
 });
 
 App.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
+  }
+});
+
+App.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
+});
+
+App.TodosActiveRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isDone');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+App.TodosDoneRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isDone');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
 
